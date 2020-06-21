d7b8b3bb8821748fd11c1c40a815c8c0a578a673
Jim <Jim Kim>
2020-06-21 20:30:50 +0900

    v-if='변수명 등' this.(이벤트 이름, 전달할 데이터) exercise

--- src/login.vue
+++ src/login.vue
@@ -2,17 +2,34 @@
     <div class="login-box">
         <h1 class="login-box--title">Login</h1>
         <namebox></namebox>
+        <namebox v-if="registerPageFlag"></namebox>
         <passwordbox></passwordbox>
+        <passwordbox v-if="registerPageFlag"></passwordbox>
         <buttons></buttons>
+        <register-link @changeRegister="registerPageFlag=$event"></register-link>
+        <!-- @리스닝할 자식 컴포넌트의 이벤트 이름 ="동작" 여기서는 자식 컴포넌트에서 전달받은 데이터($event)를  registerPageFlag에 넣어주고 있다-->
     </div>
 </template>
 <script>
 import namebox from './parts/namebox'
 import passwordbox from './parts/passwordbox'
 import buttons from './parts/buttons'
+import registerLink from './parts/registerLink'
 
 export default {
-    components: {namebox, passwordbox, buttons}
+    components: {namebox, passwordbox, buttons, registerLink},
+
+    data() {
+        return {
+            registerPageFlag: false
+        }
+    },
+
+    methods: {
+        changeRegisterFlag() {
+            this.registerLink = true;
+        }
+    }
     
 }
 </script>
---
+++ src/parts/registerLink.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="login-box--link">
+        <input type="button"  @click="changeRegisterFlag" >Sign up</input>
+    </div>
+</template>
+<script>
+export default {
+    methods: {
+        changeRegisterFlag () {
+
+            //this.$emit('부모 컴포넌트에서 리스닝할 이벤트 이름', 전달할 데이터)
+            this.$emit('changeRegister', true);
+            console.log("test");
+        }
+    }
+
+    
+}
+</script>
